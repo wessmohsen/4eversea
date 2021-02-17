8d906dac3484a19a344763f1c42982a15044e860
wessmohsen <wess.mohsen@gmail.com>
2021-02-17 23:10:17 +0200

    update

--- wp-config.php
+++ wp-config.php
@@ -23,10 +23,10 @@
 define( 'DB_NAME', 'hurghada_yalla' );
 
 /** MySQL database username */
-define( 'DB_USER', 'hurghada_yallusr' );
+define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'kKvVaSNT21MT' );
+define( 'DB_PASSWORD', '' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
