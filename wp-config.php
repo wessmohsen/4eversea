74f67286162e217db689f335abd8016324dcea3e
wessmohsen <wess.mohsen@gmail.com>
2021-02-17 23:44:17 +0200

    update

--- wp-config.php
+++ wp-config.php
@@ -10,7 +10,7 @@
  *
  * * MySQL settings
  * * Secret keys
- * * Database table prefix
+ * * Database table prefix 
  * * ABSPATH
  *
  * @link https://wordpress.org/support/article/editing-wp-config-php/
